b9e588926f6eb661234939c84019cb88f1cccd57
Daniel Flores <danielflores@Daniels-MacBook-Pro.local>
2017-12-09 23:05:18 +0100

    #1_scaffolding

---
+++ app/src/main/java/se/dafl/lastlettertrip/MainActivity.java
@@ -0,0 +1,13 @@
+package se.dafl.lastlettertrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
